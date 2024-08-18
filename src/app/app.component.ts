cd6ce3e602cc82af4bc8757793e7fb416a258da2
SujithSj <sujithloginout@gmail.com>
2024-08-18 23:35:13 +0530

    record adding initialized

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { RecordComponent } from './components/record/record.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, FooterComponent],
+  imports: [RouterOutlet, HomeComponent, FooterComponent, RecordComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
